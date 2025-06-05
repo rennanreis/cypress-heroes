314a2117f7dd18869aaa12d4be97d252938f405e
Rennan Reis <rennanreis@me.com>
2025-06-05 19:33:34 -0300

    test: validate login modal fields are rendered

--- client/cypress/e2e/heroes.cy.ts
+++ client/cypress/e2e/heroes.cy.ts
@@ -1,17 +1,25 @@
 describe('Heroes App - Basic Features', () => {
   beforeEach(() => {
+    // go to the main page
     cy.visit('/heroes');
   });
 
-  it('should display the hero list', () => {
-   
+  it('should display the login button and open modal', () => {
+    // check if login button is visible and click it
+    cy.contains('Login', { timeout: 5000 })
+      .should('exist')
+      .and('be.visible')
+      .click({ force: true });
+
+    // check if login modal is displayed
+    cy.get('form').should('exist');
   });
 
   it('should allow adding a new hero', () => {
-   
+    // to be implemented
   });
 
   it('should allow deleting a hero', () => {
- 
+    // to be implemented
   });
 });
