dec11e02a70d543b1603f0c62f1f8114095fa33f
Rennan Reis <rennanreis@me.com>
2025-06-05 19:02:18 -0300

    chore: setup backend, seed database and add initial Cypress test structure

---
+++ client/cypress/e2e/heroes.cy.ts
@@ -0,0 +1,17 @@
+describe('Heroes App - Basic Features', () => {
+  beforeEach(() => {
+    cy.visit('/heroes');
+  });
+
+  it('should display the hero list', () => {
+   
+  });
+
+  it('should allow adding a new hero', () => {
+   
+  });
+
+  it('should allow deleting a hero', () => {
+ 
+  });
+});
